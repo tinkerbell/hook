9409643e8b46ef8a23a76b3c5fceb3e3f91b1552
Jacob Weinstock <jakobweinstock@gmail.com>
2023-01-24 20:46:18 -0700

    Fix dhcp.sh to be posix compliant:
    As this script is made to run inside linuxkit/dhcpcd:v0.8
    it needs to be posix compliant and wasn't before this.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- files/dhcp.sh
+++ files/dhcp.sh
@@ -7,33 +7,25 @@
 # false: run the dhcp client as a service
 set -x
 
-parse_cmdline() {
-	c=$(cat /proc/cmdline)
-	c="${c##*"$1"=}"
-	c="${c%% *}"
-	echo "$c"
-}
-
 run_dhcp_client() {
 	one_shot="$1"
 	al="eth*"
 
-	vlan_id=$(parse_cmdline vlan_id)
-	if [ -n "$vlan_id" ]; then
+	if grep "vlan_id" /proc/cmdline; then
 		al="eth*.*"
+		# Boots send kernel command line parameter "ip=dhcp", this causes the system to configure the network interface(s) with DHCP.
+		# When an environment's network configuration has this machine connected a trunked interface with a default/native VLAN, the
+		# interface will be configured on the default/native VLAN because we haven't yet configured the VLAN interface. Boots will respond
+		# to this DHCP request because in this scenario it is not VLAN aware. Also in this scenario, the machine will end up being configured
+		# with 2 default routes. To resolve this, we remove the default route and IP that kernel added and let the dhcpcd handle setting the route.
+		ip route del default || true
+		ipa=$(ip -4 -o addr show dev eth0 | awk '{print $4}')
+		ip addr del dev eth0 "$ipa" || true
 	fi
 
-	# Boots send kernel command line parameter "ip=dhcp", this causes the system to configure the network interface(s) with DHCP.
-	# When an environment's network configuration has this machine connected a trunked interface with a default/native VLAN, the
-	# interface will be configured on the default/native VLAN because we haven't yet configured the VLAN interface. Boots will respond
-	# to this DHCP request because in this scenario it is not VLAN aware. Also in this scenario, the machine will end up being configured
-	# with 2 default routes. To resolve this, we remove the default route and IP that kernel added and let the dhcpcd handle setting the route.
-	ip route del default || true
-	ipa=$(ip -4 -o addr show dev eth0 | awk '{print $4}')
-	ip addr del dev eth0 "$ipa" || true
-
 	if [ "$one_shot" = "true" ]; then
-		/sbin/dhcpcd --nobackground -f /dhcpcd.conf --allowinterfaces "${al}" -1
+		# always return true for the one shot dhcp call so it doesn't block Hook from starting up.
+		/sbin/dhcpcd --nobackground -f /dhcpcd.conf --allowinterfaces "${al}" -1 || true
 	else
 		/sbin/dhcpcd --nobackground -f /dhcpcd.conf --allowinterfaces "${al}"
 	fi

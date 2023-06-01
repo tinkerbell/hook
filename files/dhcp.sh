446e2f40ed41f6f0e8e889eb3b18659e085852d0
Jacob Weinstock <jakobweinstock@gmail.com>
2023-06-01 08:42:44 -0600

    Update vlan_id parse based on linting feedback.
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- files/dhcp.sh
+++ files/dhcp.sh
@@ -11,7 +11,7 @@ run_dhcp_client() {
 	one_shot="$1"
 	al="eth*"
 
-	vlan_id=$(cat /proc/cmdline | sed -n 's/.*vlan_id=\([0-9]*\).*/\1/p')
+	vlan_id=$(sed -n 's/.*vlan_id=\([0-9]*\).*/\1/p' /proc/cmdline)
 	if [ -n "$vlan_id" ]; then
 		al="eth*.*"
 	fi

e3ef9c62289886ce89642d2654120085e1ae5ee2
Ricardo Pardini <ricardo@pardini.net>
2024-05-19 09:45:14 +0200

    kernel: armbian: don't flood output with tar's verbose option
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- bash/kernel/kernel_armbian.sh
+++ bash/kernel/kernel_armbian.sh
@@ -94,7 +94,7 @@ function calculate_kernel_version_armbian() {
 		# Important: this tarball needs to have permissions for the root directory included! Otherwise linuxkit rootfs will have the wrong permissions on / (root)
 		WORKDIR /armbian/modules_only
 		RUN mv /armbian/image/lib /armbian/modules_only/
-		RUN tar -cvf /armbian/output/kernel.tar .
+		RUN tar -cf /armbian/output/kernel.tar .
 
 		# Create a tarball with the dtbs in usr/lib/linux-image-*
 		RUN { cd usr/lib/linux-image-* || { echo "No DTBS for this arch, empty tar..." && mkdir -p usr/lib/linux-image-no-dtbs && cd usr/lib/linux-image-* ; } ; }  && pwd && du -h -d 1 . && tar -czvf /armbian/output/dtbs.tar.gz . && ls -lah /armbian/output/dtbs.tar.gz

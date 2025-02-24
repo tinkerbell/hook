5781767812b71d5b710b33b139ca362ce2d5bc7d
Ricardo Pardini <ricardo@pardini.net>
2025-02-24 20:13:55 -0300

    bootable: grub: implement grabbing GRUB bins from Linuxkit into EFI/BOOT
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- bash/bootable/grub.sh
+++ bash/bootable/grub.sh
@@ -6,11 +6,13 @@ function list_bootable_grub() {
 }
 
 function build_bootable_grub() {
+	: "${kernel_info['DOCKER_ARCH']:?"kernel_info['DOCKER_ARCH'] is unset"}"
 	: "${bootable_info['INVENTORY_ID']:?"bootable_info['INVENTORY_ID'] is unset"}"
 	: "${OUTPUT_ID:?"OUTPUT_ID is unset"}"
 
 	declare hook_id="${bootable_info['INVENTORY_ID']}"
 	declare bootable_img="bootable_grub_${OUTPUT_ID}.img"
+	declare kernel_command_line="" # @TODO: common stuff for tink, etc
 
 	declare has_dtbs="${bootable_info['DTB']}"
 	[[ -z "${has_dtbs}" ]] && has_dtbs="no"
@@ -52,11 +54,15 @@ function build_bootable_grub() {
 	if [[ "${has_dtbs}" == "yes" ]]; then
 		mkdir -p "${fat32_root_dir}/dtb"
 		tar -C "${fat32_root_dir}/dtb" --strip-components=1 -xzf "out/hook/${hook_files["dtbs"]}"
-		tree "${fat32_root_dir}"
 	fi
 
-	### @TODO: obtain and write grub binary to EFI directory
-	### @TODO: write grub.cfg
+	# Grab the GRUB binaries from the LinuxKit Docker images
+	declare grub_arch="${kernel_info['DOCKER_ARCH']}"
+	declare grub_linuxkit_image="linuxkit/grub-dev:4184bd7644a0edf73d4fe8a55171fe06f4b4d738" # See https://github.com/linuxkit/linuxkit/blob/master/tools/grub/Dockerfile for the latest
+	declare fat32_efi_dir="${fat32_root_dir}/EFI/BOOT"
+	mkdir -p "${fat32_efi_dir}"
+
+	download_grub_binaries_from_linuxkit_docker_images "${fat32_efi_dir}" "${grub_arch}" "${grub_linuxkit_image}"
 
 	# Show the state
 	du -h -d 1 "${bootable_base_dir}"
@@ -74,5 +80,28 @@ function build_bootable_grub() {
 	output_bootable_media "${bootable_base_dir}/${bootable_img}" "hook-bootable-grub-${OUTPUT_ID}.img"
 
 	return 0
+}
 
+function download_grub_binaries_from_linuxkit_docker_images() {
+	declare output_dir="${1}"
+	declare arch="${2}"
+	declare image="${3}"
+	log info "Grabbing GRUB bins for arch '${arch}' from image '${image}'..."
+
+	# Lets create a Dockerfile that will be used to obtain the artifacts needed
+	declare -g grub_grabber_dockerfile="bootable/Dockerfile.autogen.grub_grabber"
+	log info "Creating Dockerfile '${grub_grabber_dockerfile}'... "
+
+	cat <<- GRUB_GRABBER_DOCKERFILE > "${grub_grabber_dockerfile}"
+		FROM --platform=linux/${arch} ${image} AS grub-build-${arch}
+		FROM scratch
+		COPY --from=grub-build-${arch} /*.EFI /
+	GRUB_GRABBER_DOCKERFILE
+
+	# Now, build the Dockerfile and output the fat32 image directly
+	log info "Building Dockerfile for GRUB grabber and outputting directly to '${output_dir}'..."
+	docker buildx build --output "type=local,dest=${output_dir}" "--progress=${DOCKER_BUILDX_PROGRESS_TYPE}" -f "${grub_grabber_dockerfile}" bootable
+
+	log info "Done, GRUB binaries are in ${output_dir}"
+	tree "${output_dir}"
 }
